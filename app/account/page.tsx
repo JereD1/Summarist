c070e343ce5003553ab844e74a91ecd80b7b2df8
MereaD <jereadgaming2@gmail.com>
2024-08-24 09:32:13 +0100

    image fix

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -8,25 +8,24 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const AccountPage = () => {
   return (
     <Router>
-    <div className='mx-60 p-4'>
-      <div className='mb-4'>
-       <h1 className='font-semibold text-xl'> Selected just for you</h1>
+      <div className='mx-60 p-4'>
+        <div className='mb-4'>
+          <h1 className='font-semibold text-xl'>Selected just for you</h1>
+        </div>
+        <SelectedBook />
+        <div className='mt-4'>
+          <h1 className='font-semibold text-xl mb-3'>Recommended For You</h1>
+          <h3>We think you&#39;ll like these</h3> {/* Escaped the single quote */}
+          <RecommendedBook />
+        </div>
+        <div className='mt-4'>
+          <h1 className='font-semibold text-xl mt-4 mb-3'>Suggested Books</h1>
+          <h3>Browse those books</h3>
+          <SuggestedBooks /> 
+        </div>
       </div>
-      <SelectedBook />
-      <div className='mt-4'>
-        <h1 className='font-semibold text-xl mb-3'>Recommended For You</h1>
-        <h3>We think you'll like these</h3>
-        < RecommendedBook />
-      </div>
-      <div className='mt-4'>
-        <h1 className='font-semibold text-xl mt-4 mb-3'>Suggested Books</h1>
-        <h3>Browse those books</h3>
-        <SuggestedBooks /> 
-      </div>
-    </div>
     </Router>
   );
 }
 
 export default AccountPage;
-
